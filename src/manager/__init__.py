0f3020585cf174676eacb616f5db0d2e6ef13364
Michael Tremer <michael.tremer@ipfire.org>
2017-10-08 15:00:05 +0100

    Regularly cleanup files
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- src/manager/__init__.py
+++ src/manager/__init__.py
@@ -1,25 +1,6 @@
 #!/usr/bin/python
 
-from . import base
-
 from .builds       import BuildsFailedRestartEvent, CheckBuildDependenciesEvent
 from .builds       import CreateTestBuildsEvent, DistEvent
 from .repositories import RepositoriesUpdateEvent
 from .sources      import SourcesPullEvent
-
-
-# Events that do not fit anywhere else.
-
-class CleanupFilesEvent(base.Event):
-	"""
-		Removes all files that are not needed anymore.
-		(scratch builds, logs, etc.)
-	"""
-	# Run once in 5 minutes.
-	interval = 300
-
-	# Intermediate priority.
-	priority = 5
-
-	def run(self):
-		self.pakfire.cleanup_files()
