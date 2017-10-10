9729c5b3033369cb1ec4eeb0e870d0fd7c71a1a7
Michael Tremer <michael.tremer@ipfire.org>
2017-10-10 15:57:31 +0100

    Move remastering repositories
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- src/manager/__init__.py
+++ src/manager/__init__.py
@@ -2,4 +2,3 @@
 
 from .builds       import BuildsFailedRestartEvent, CheckBuildDependenciesEvent
 from .builds       import CreateTestBuildsEvent, DistEvent
-from .repositories import RepositoriesUpdateEvent
